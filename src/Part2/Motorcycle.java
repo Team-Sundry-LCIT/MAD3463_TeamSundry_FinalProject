ad2d88298a8cc19edb050ed144425d7d31d268cf
Alice <c0874203@mylambton.ca>
2022-11-17 13:53:27 -0500

    init Motorcycle class and variables

--- src/Part2/Motorcycle.java
+++ src/Part2/Motorcycle.java
@@ -1,4 +1,12 @@
 package Part2;
 
+
 public class Motorcycle {
+    private boolean sideCar = false;
+    private String make;
+    private String plate;
+    private String color;
+    private String category;
+
+
 }
