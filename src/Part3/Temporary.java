ef91e71c77d15e46cbfda234bc56f830cca3020b
Alice <c0874203@mylambton.ca>
2022-11-17 13:58:34 -0500

    Implement Temporary interface

--- src/Part3/Temporary.java
+++ src/Part3/Temporary.java
@@ -1,5 +1,10 @@
 package Part3;
 
+interface TemporaryInterface {
+    int hourlySalary = 0;
+    int accumulatedHours = 0;
+    double accumulatedSalary();
+}
 public class Temporary {
     private int hourlySalary = 0;
     private int accumulatedHours = 0;
