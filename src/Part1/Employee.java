64fe273aa707b3ee9761623705a45bcbc4f3ceaf
Tilak <c0868747@mylambton.ca>
2022-11-17 19:18:39 -0500

    employee abstract class with setter/getter

--- src/Part1/Employee.java
+++ src/Part1/Employee.java
@@ -1,7 +1,49 @@
 package Part1;
 
-public class Employee {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+abstract class Employee {
+    private int name;
+    private int birthYear;
+    private int age;
+    private int monthlyIncome;
+    private int rate;
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getMonthlyIncome() {
+        return monthlyIncome;
+    }
+
+    public void setMonthlyIncome(int monthlyIncome) {
+        this.monthlyIncome = monthlyIncome;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
     }
 }
