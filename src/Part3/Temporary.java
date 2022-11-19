71a2bcc71ce66a9077cf37ea2afe93bbcb9aaa19
Alice <c0874203@mylambton.ca>
2022-11-19 15:37:56 -0500

    Change to use EmployeeContract interface.

--- src/Part3/Temporary.java
+++ src/Part3/Temporary.java
@@ -1,11 +1,6 @@
 package Part3;
 
-interface TemporaryInterface {
-    int hourlySalary = 0;
-    int accumulatedHours = 0;
-    double accumulatedSalary();
-}
-public class Temporary implements TemporaryInterface {
+public class Temporary implements EmployeeContract  {
     private int hourlySalary = 0;
     private int accumulatedHours = 0;
 
