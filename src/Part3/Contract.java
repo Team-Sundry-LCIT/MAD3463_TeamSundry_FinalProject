56511c064d89b70efd5f3e1d9c82329c1a3a45eb
0871063 <c0871063@mylambton.ca>
2022-11-19 12:10:08 -0500

    Test 1 and Test 2 Output

--- src/Part3/Contract.java
+++ src/Part3/Contract.java
@@ -1,4 +1,12 @@
 package Part3;
 
 interface Contract {
+
+    public void signContract(Contract contract);
+    public String contractInfo();
 }
+//
+//public class Contract1(){
+//    public Contract1() {
+//    }
+//}
