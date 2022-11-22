8f2fdfde060507b079cbe3e4a134d6b70930c176
0871063 <c0871063@mylambton.ca>
2022-11-22 14:20:43 -0500

    Print Employee Contact Details

--- src/Part3/Permanent.java
+++ src/Part3/Permanent.java
@@ -1,11 +1,6 @@
 package Part3;
 
 public class Permanent implements EmployeeContract {
-//    @Override
-    public String contractInfo() {
-        return null;
-    }
-
     @Override
     public double accumulatedSalary() {
         return EmployeeContract.super.accumulatedSalary();
