c3cd7300c46aa1e1ebbd19cd1a6a78bdf7b631dd
0871063 <c0871063@mylambton.ca>
2022-11-22 18:00:50 -0500

    Documentation

--- src/Part3/Permanent.java
+++ src/Part3/Permanent.java
@@ -3,7 +3,7 @@ package Part3;
 public class Permanent implements EmployeeContract {
     @Override
     public double accumulatedSalary() {
-        return EmployeeContract.super.accumulatedSalary();
+        return 0.0;
     }
 
     //    @Override
