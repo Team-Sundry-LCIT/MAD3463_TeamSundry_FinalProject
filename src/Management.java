2e49601b21049261edf89a1224de8e1ca67c1128
0871063 <c0871063@mylambton.ca>
2022-11-23 14:29:45 -0500

    Management File Changes

--- src/Management.java
+++ src/Management.java
@@ -36,12 +36,12 @@ public class Management {
 
     public void hire_test2() {
         employees = new ArrayList<>();
-//        employees.add(new Manager("Serge", 1985, 30, 4, vt1));
-//        employees.add(new Manager("Cindy",1974, 20,  6, vt2));
-//        employees.add(new Programmer("Paul",  1993,  3, 75, vt3));
+        employees.add(new Manager("Serge", 1985, 4, 30, vt1));
+        employees.add(new Manager("Cindy",1974, 6,  20, vt2));
+        employees.add(new Programmer("Paul",  1993,  3, 75, vt3));
         employees.add(new Tester("Pierre", 1987, 124, 50, m2));
-//        employees.add(new Programmer("Matt", 1981, 5, 110, vt4));
-//        employees.add(new Programmer("Sarah", 1986, 3,30, vt3));
+        employees.add(new Programmer("Matt", 1981, 5, 110, vt4));
+        employees.add(new Programmer("Sarah", 1986, 3,30, vt3));
 
         for( Employee item : employees ){
             System.out.println("-------------------------");
@@ -53,34 +53,33 @@ public class Management {
     public void hire_test3(){
         employees = new ArrayList<>();
 
-//        Manager Serge = new Manager("Serge",1985,30,4,vt1);
-//        Manager Cindy = new Manager("Cindy",1974,20,6,80,vt2);
-//        Programmer Paul = new Programmer("Paul",1983,3,75,m1);
+        Manager Serge = new Manager("Serge",1985,4,30,vt1);
+        Manager Cindy = new Manager("Cindy",1974,6,20,80,vt2);
+        Programmer Paul = new Programmer("Paul",1983,3,75,m1);
         Tester Pierre = new Tester("Pierre", 1987, 124, 50, m2);
-//        Programmer Matt = new Programmer("Matt",1981,5,110,vt4);
-//        Programmer Sarah = new Programmer("Sarah",1986,3,vt3);
+        Programmer Matt = new Programmer("Matt",1981,5,100,vt4);
+        Programmer Sarah = new Programmer("Sarah",1986,3,100,vt3);
 
-//        Permanent p1 = new Permanent(2,true,6000,200,17);
-//        Permanent p2 = new Permanent(3,false,5000,150,19);
+        Permanent p1 = new Permanent(2,true,6000,200,17);
+        Permanent p2 = new Permanent(3,false,5000,150,19);
         Temporary t1 = new Temporary(60,30);
         Temporary t2 = new Temporary(50,60);
-//        Permanent p3 = new Permanent(1,true,4000,100,20);
-//        Permanent p4 = new Permanent(4,true,4500,100,19);
+        Permanent p3 = new Permanent(1,true,4000,100,20);
+        Permanent p4 = new Permanent(4,true,4500,100,19);
 
-//        Serge.signContract(p1);
-//        Cindy.signContract(p2);
-//        Paul.signContract(t1);
-//        Matt.signContract( t2);
-//        Pierre.signContract(p3);
-//        Sarah.signContract( p4);
-       Pierre.signContract(t1);
+        Serge.signContract(p1);
+        Cindy.signContract(p2);
+        Paul.signContract(t1);
+        Matt.signContract( t2);
+        Pierre.signContract(p3);
+        Sarah.signContract( p4);
 
-//        employees.add(Serge);
-//        employees.add(Cindy);
-//        employees.add(Paul);
+        employees.add(Serge);
+        employees.add(Cindy);
+        employees.add(Paul);
         employees.add(Pierre);
-//        employees.add(Matt);
-//        employees.add(Sarah);
+        employees.add(Matt);
+        employees.add(Sarah);
 
         for( Employee item : employees ){
             System.out.println("-------------------------");
