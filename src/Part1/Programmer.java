67a2350a4c5023d111452fea29c5f3f50e196ea7
BaoTrieuThai206 <c0866206@mylambton.ca>
2022-11-23 11:19:38 -0500

    Programmer - extends from Employee, create constructor, get annual income and print

--- src/Part1/Programmer.java
+++ src/Part1/Programmer.java
@@ -1,6 +1,6 @@
 package Part1;
 
-public class Programmer {
+public class Programmer extends Employee{
     private int projects;
     private final int gainFactorProjects = 200;
 
@@ -10,5 +10,25 @@ public class Programmer {
 
     public void setProjects(int projects) {
         this.projects = projects;
+    }
+
+    public Programmer(String name, int birthYear, int projects, int rate) {
+        super(name, birthYear, rate);
+        this.projects = projects;
+        printData();
+    }
+
+    @Override
+    public double getAnnualIncome() {
+        double baseYearlyIncome = (getMonthlyIncome() * 12) * getRate()/100.0;
+        double bonus = gainFactorProjects * getProjects();
+        return baseYearlyIncome + bonus;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                " has completed  " + getProjects() + " projects." +
+                "\nHis/Her estimated annual income is " + getAnnualIncome();
     }
 }
