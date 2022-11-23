08e8d9ffa9af9dfc8a58d5c51e60cee450e576fd
BaoTrieuThai206 <c0866206@mylambton.ca>
2022-11-23 11:17:20 -0500

    Programmer - create variables, contants, get, set method

--- src/Part1/Programmer.java
+++ src/Part1/Programmer.java
@@ -1,4 +1,14 @@
 package Part1;
 
 public class Programmer {
+    private int projects;
+    private final int gainFactorProjects = 200;
+
+    public int getProjects() {
+        return projects;
+    }
+
+    public void setProjects(int projects) {
+        this.projects = projects;
+    }
 }
